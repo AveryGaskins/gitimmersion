4b769bd1e16d6b8a31681de8df55f5221d48faa6
Avery Gaskins <agaskin7@uncc.edu>
2015-11-17 20:34:22 +0000

    Merged master fixed conflict.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,14 @@
+<<<<<<< HEAD
+require 'greeter'
+
+# Default is World
+name = ARGV.first || "World"
+
+greeter = Greeter.new(name)
+require 'greeter'
+
 puts "What's your name"
 my_name = gets.strip
 
-puts "Hello, #{my_name}!"
+greeter = Greeter.new(my_name)
+puts greeter.greet
